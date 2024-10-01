44e5931978737f9b21b6eb2bda5e739a17606057
angelikatot <angelika.tot@hotmail.com>
2024-10-01 11:40:12 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { useState } from 'react';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+      });
+
+      // only contacts with nummbers
+      const filteredContacts = data.filter(contact => contact.phoneNumbers && contact.phoneNumbers.length > 0);
+      setContacts(filteredContacts);
+    }
+  }
+
+  const renderItem = ({ item }) => (
+    <View style={styles.contactItem}>
+      <Text>{item.name}</Text>
+      <Text>{item.phoneNumbers[0].number}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Get Contacts" onPress={getContacts} />
+      {contacts.length > 0 ? (
+        <FlatList
+          data={contacts}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+        />
+      ) : (
+        <Text>No contacts fetched yet</Text>
+      )}
     </View>
   );
 }
@@ -16,5 +49,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  contactItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
   },
 });
